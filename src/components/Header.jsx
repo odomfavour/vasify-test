dcc0518ff0ce0db0024d5b33584e59cb00905a55
odomfavour <ogochukwuodom@gmail.com>
2023-01-15 16:53:16 +0100

    ch: fixed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {FaBars} from 'react-icons/fa'
+import { FaBars } from "react-icons/fa";
 const Header = () => {
   return (
     <div className="fixed top-0 z-40 w-full bg-white">
       <div className="w-11/12 mx-auto">
         <div className="flex justify-between py-3 items-center">
-            <FaBars className="text-3xl" role="button"/>
+          <div className="md:hidden block">
+            <FaBars className="text-3xl" role="button" />
+          </div>
           <div className="md:w-1/2">
             <Link to="/" className="text-xl font-semibold">
               Vasify
             </Link>
           </div>
-          <button className="bg-[#0119b7] text-white px-2 py-1 rounded-sm">Get App</button>
+          <div className="md:hidden block">
+            <button className="bg-[#0119b7] text-white px-2 py-1 rounded-sm">
+              Get App
+            </button>
+          </div>
           <div className="w-1/2 hidden md:block">
             <ul className="flex">
               <li className="mr-6">
